450b0d36061079ec212d4f901731dd9dd6ceb328
Poulpy <paul.repain@yahoo.com>
2021-12-28 17:32:33 +0100

    Read resource file and create a matrix

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <sstream>
+#include <fstream>
 
 #include <Eigen/SparseCore>
+
+#define TEST_FILEPATH "../resources/email-Eu-core.txt"
 
 using Eigen::SparseMatrix;
 using namespace std;
 
-typedef Eigen::Triplet<double> T;
+typedef Eigen::Triplet<int> T;
 
 /**
  * d = 0.15, damping factor
@@ -15,34 +18,55 @@ typedef Eigen::Triplet<double> T;
  */
 int main(int argc, char **argv) {
 
-    if (argc < 4) {
-        std::cout << "Less than 4 arguments, run with :" << std::endl;
-        std::cout << "./a.out <d> <max> <eps>" << std::endl;
-
-        return 0;
-    }
-
     double d;
     unsigned max;
     double eps;
 
-    stringstream(argv[1]) >> d;
-    stringstream(argv[2]) >> max;
-    stringstream(argv[3]) >> eps;
+    if (argc < 4) {
+        d = 0.15;
+        max = 100;
+        eps = 0.000001;
+    } else {
+        stringstream(argv[1]) >> d;
+        stringstream(argv[2]) >> max;
+        stringstream(argv[3]) >> eps;
+    }
+
 
     std::cout << d << std::endl;
     std::cout << max << std::endl;
     std::cout << eps << std::endl;
 
+    
+    // File pointer
+    std::ifstream fin(TEST_FILEPATH);
+  
     std::vector<T> tripletList;
-    tripletList.reserve(4);
-    tripletList.push_back(T(0,0, 3.0));
-    tripletList.push_back(T(1,0, 2.5));
-    tripletList.push_back(T(0,1, -1.0));
-    tripletList.push_back(T(1,1, 56.4));
+    tripletList.reserve(2000);
 
-    SparseMatrix<double> A(2, 2);
+    // https://www.geeksforgeeks.org/csv-file-management-using-c/
+    // Read the Data from the file
+    // as String Vector
+    vector<string> row;
+    string line, word, temp;
+    int maximum = 0;
+    int i = 0;
+    int n1 = 0, n2 = 0;
+
+    while (fin >> n1 >> n2) {
+        maximum = std::max(maximum, n1);
+        maximum = std::max(maximum, n2);
+
+        tripletList.push_back(T(n1, n2, 1));
+        tripletList.push_back(T(n2, n1, 1));
+    }
+
+    cout << "Maximum is " << maximum << endl;
+
+    SparseMatrix<unsigned> A(maximum + 1, maximum + 1);
+
     A.setFromTriplets(tripletList.begin(), tripletList.end());
-    std::cout << A << std::endl;
+    //std::cout << A << std::endl;
+    fin.close();
 }
 
